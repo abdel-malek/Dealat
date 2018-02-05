d899c9c409cf53cef38c0978a9882f644202ffe7
alaa-jad <alaa@tradinos.com>
2018-02-05 16:16:43 +0200

    Profile page + fixes
    -add nav arrows for sub categories overflow
    -show /hide social media sidebar
    -hide aside ads in small devices
    -fix home responsive

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $(function () {
 		$(this).addClass("selected");
 	});
 
-	$(".card .overlay").click(function () {
-		$(".products .row .ad").addClass("invisible");
+	$(".home-page .card .card-img-top,.home-page .card .overlay,.profile-page .favorites .card-left, .search-page .card-left").click(function () {
+//		$(".products .row .ad").addClass("invisible");
 		$("#card-modal").modal("show");
 		//		setTimeout(function () {
 		//$(window).trigger('resize');
@@ -87,32 +87,96 @@ $(function () {
 		});
 	});
 
-		$(".card .fav .icon").mouseenter(function () {
-			$(this).css("color", "orange");
-		});
-		$(".card .fav .icon").mouseleave(function () {
-			if ($(this).data("added") === 0) {
+	$(".profile-page .rating .rate-group").each(function () {
+		if ($(this).data("value") <= userRateValue) {
+			$(this).children("label").css("color", "#FFCC36");
+		} else {
+			$(this).children("label").css("color", "#ddd");
+		}
+	});
+
+	//add item to favorite
+	$(".card .fav .icon").mouseenter(function () {
+		$(this).css("color", "orange");
+	});
+	$(".card .fav .icon").mouseleave(function () {
+		if ($(this).data("added") === 0) {
 			$(this).css("color", "#999");
 		} else if ($(this).data("added") === 1) {
 			$(this).css("color", "orange");
 		}
-		});
-	//add item to favorite
+	});
+
 	$(".card .fav .icon").click(function () {
 		if ($(this).data("added") === 0) {
 			$(this).html('<i class="fas fa-star fa-2x"></i>');
 			$(this).css("color", "orange");
 			$(this).data("added", 1);
 			//			$(this).siblings(".text").text("Add to favorites");
-			$(this).attr("title","Remove from favorites");
+			$(this).attr("title", "Remove from favorites");
 		} else if ($(this).data("added") === 1) {
 			$(this).html('<i class="far fa-star fa-2x"></i>');
 			$(this).css("color", "#999");
 			$(this).data("added", 0);
 			//			$(this).siblings(".text").text("Remove from favorites");
-			$(this).attr("title","Add to favorites");
+			$(this).attr("title", "Add to favorites");
 		}
 
+	});
+
+	//menu nav left and right arrows
+	if ($(".controls ul").width() > $(".controls").width()) {
+		$(".nav-scroller.next").removeClass("d-none");
+	}
+
+	$(".nav-scroller.next").click(function () {
+		$(".nav-scroller.prev").removeClass("d-none");
+		if (($(".controls ul").offset().left + $(".controls ul").width()) <= ($(".controls").offset().left + $(".controls").width())) {
+			$(this).addClass("d-none");
+			return;
+		}
+		$(".controls ul").css({
+			"margin-left": "-=100px"
+		});
+		if (($(".controls ul").offset().left + $(".controls ul").width()) <= ($(".controls").offset().left + $(".controls").width() + 80)) {
+			$(this).addClass("d-none");
+		}
+	});
+
+	$(".nav-scroller.prev").click(function () {
+		$(".nav-scroller.next").removeClass("d-none");
+		if ($(".controls ul").offset().left >= $(".controls").offset().left) {
+			$(this).addClass("d-none");
+			$(".controls ul").css({
+				"margin-left": "0px"
+			});
+			return;
+		}
+		$(".controls ul").css({
+			"margin-left": "+=100px"
+		});
+
+		if ($(".controls ul").offset().left >= ($(".controls").offset().left - 100)) {
+			$(this).addClass("d-none");
+			$(".controls ul").css({
+				"margin-left": "0px"
+			});
+		}
+	});
+
+	//show/hide social media sidebar
+	$(".show-social").click(function () {
+		if ($(".social-fixed .icons").data("show") === 0) {
+			$(".social-fixed .icons").css("left", "0");
+			$(".social-fixed .icons").data("show", 1);
+			$(".social-fixed svg").removeClass("fa-angle-right");
+			$(".social-fixed svg").addClass("fa-angle-left");
+		} else {
+			$(".social-fixed .icons").css("left", "-30px");
+			$(".social-fixed .icons").data("show", 0);
+			$(".social-fixed svg").removeClass("fa-angle-left");
+			$(".social-fixed svg").addClass("fa-angle-right");
+		}
 	});
 
 });
